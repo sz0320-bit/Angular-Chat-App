c1b8205d5acfd657610acd5ae0c2999207991722
Syed Zaidi <syed.zaidi.hussain@gmail.com>
2023-05-04 08:38:27 -0400

    minor ui changes

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,15 +1,28 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MainComponent } from './main.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MainComponent} from './main.component';
+import {RouterModule, Routes} from "@angular/router";
+import {SharedModule} from "../shared/shared.module";
+import { PostComponent } from './post/post.component';
 
+
+const routes: Routes = [
+
+  {path: '', component: MainComponent},
+
+];
 
 
 @NgModule({
   declarations: [
-    MainComponent
+    MainComponent,
+    PostComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
+    SharedModule
   ]
 })
-export class MainModule { }
+export class MainModule {
+}
